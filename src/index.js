a976e7e5efb1e84b6b34db77e96551e02f811df8
Wangeci-Linet <wangecilinetthimba@gmail.com>
2025-11-26 15:13:18 +0300

    Added API integration within the JavaScript file and modified the HTML and CSS files

---
+++ src/index.js
@@ -0,0 +1,53 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let dateElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#current-temperature-icon");
+  let icon = `<img src="${response.data.condition.icon_url}"/>`;
+  iconElement.innerHTML = icon;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  dateElement.innerHTML = `${day} ${hours}:${minutes}`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+}
+
+function searchCity(city) {
+  let apiKey = "1bf42foc33aa256d79167at7004c6dac";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function citySubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  let cityElement = document.querySelector(".current-city");
+  cityElement.innerHTML = cityInputElement.value;
+  searchCity(cityInputElement.value);
+}
+
+let form = document.querySelector(".search-form");
+form.addEventListener("submit", citySubmit);
+
+searchCity("Nairobi");
